93c12ff0a8fa35b70d0f124afc62fb697450c59c
jjnietoc <juanjonietocastillo@gmail.com>
2024-03-19 00:03:59 +0700

    change: uint16 type

--- product.hpp
+++ product.hpp
@@ -11,25 +11,25 @@ class Product
 {
   protected:
     std::string productName, productCode;
-    uint16_t stock;
+    int stock;
     float price;
 
   public:
     Product() { }
     Product(const std::string & pn,
-            int s,
+            int st,
             float p) 
-    { setName(pn); setProductCode(); setStock(s); setPrice(p); };
+    { setName(pn); setProductCode(); setStock(st); setPrice(p); };
     virtual ~Product() {};
 
     void setName(const std::string & pn) { productName = pn; }; 
     virtual void setProductCode() { productCode = "NULL"; };
-    void setStock(uint16_t st) { stock = st; };
+    void setStock(int st) { stock = st; };
     void setPrice(float pr) { price = pr; };
 
     const std::string & getName() const { return productName; }
     const std::string & getCode() const { return productCode; }
-    const uint16_t & getStock() const { return stock; }
+    const int & getStock() const { return stock; }
     const float & getPrice() const { return price; }
   };
 
