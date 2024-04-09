de61d92f67d94c908a940c28f21f8aa6aa157f8a
jjnietoc <juanjonietocastillo@gmail.com>
2024-04-09 20:37:24 +0800

    add: structure for retail software

--- cppos.cpp
+++ cppos.cpp
@@ -3,16 +3,22 @@
 
 int main(void) {
   std::cout << "cpPOS v0.1.1\n-------------\n" << 
-    "Select what interface to use:\nPress (1) for PRODUCTS or (2) for USERS" << std::endl;
+    "Select what interface to use:\nPress (1) for STOCK or (2) for USERS or (3) for RETAIL" << std::endl;
   int ans;
   std::cin >> ans;
   switch(ans) {
     case 1: {
+      // needs authentication
       stockLoop();
       break;
     }
     case 2: {
       userLoop();
+      break;
+    }
+    case 3: {
+      // needs authentication
+      // retail
       break;
     }
   }
