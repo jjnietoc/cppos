449009138ecbcadecab4023ad7d7a3078a6dca7c
jjnietoc <juanjonietocastillo@gmail.com>
2024-04-09 20:58:15 +0800

    add: function declarations

---
+++ retail.hpp
@@ -0,0 +1,14 @@
+// header file for retail software
+
+#ifndef RETAIL_HPP
+#define RETAIL_HPP
+
+#include "user.hpp"
+#include "stock.hpp"
+
+void addToCart();
+int change(int a, int b);
+void selectItem();
+void searchItem(std::string query);
+
+#endif
